d5897b31fb5bf53ba7b4ed6748422e5134d0196f
modos189 <git@modos189.ru>
2022-04-30 19:19:54 +0300

    export helpers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 // @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3
 
 import { Manager } from './manager.js';
-import * as helpers from '../src/helpers.js';
-export { Manager, helpers };
+import {
+    parseMeta,
+    ajaxGet,
+    getUniqId,
+    getUID,
+    check_meta_match_pattern,
+    wait,
+    clearWait
+} from './helpers.js';
+
+export {
+    Manager,
+    parseMeta,
+    ajaxGet,
+    getUniqId,
+    getUID,
+    check_meta_match_pattern,
+    wait,
+    clearWait
+};
 
 module.exports = {
-    Manager: Manager,
-    helpers: helpers
+    Manager: Manager
 };
