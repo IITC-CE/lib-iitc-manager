bbd3b238091b1a0a040b775f601b99f18960ab14
modos189 <git@modos189.ru>
2022-12-28 22:33:01 +0300

    Fixing the migration

--- src/migrations.js
+++ src/migrations.js
@@ -1,40 +1,54 @@
 // @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3
 
-import { isSet } from './helpers.js';
+import { isSet, getUID } from './helpers.js';
 
 export function number_of_migrations() {
     return migrates.length;
 }
 
-const migrates = [migration_0001, migration_0002];
+const migrates = [migration_0001, migration_0002, migration_0003];
 
 export async function migrate(storage) {
-    const local = await storage.get(['release_plugins_flat', 'test_plugins_flat', 'local_plugins_flat', 'lastversion', 'storage_version']);
+    const storage_plugins_flat = await storage.get([
+        'release_plugins_flat',
+        'beta_plugins_flat',
+        'custom_plugins_flat',
+        'test_plugins_flat',
+        'local_plugins_flat',
+    ]);
+    const storage_plugins_user = await storage.get([
+        'release_plugins_user',
+        'beta_plugins_user',
+        'custom_plugins_user',
+        'test_plugins_user',
+        'local_plugins_user',
+    ]);
+    const storage_misc = await storage.get(['channel', 'network_host', 'lastversion', 'storage_version']);
 
-    if (!isSet(local['storage_version']) && isSet(local['lastversion'])) {
-        local['storage_version'] = 0;
+    if (!isSet(storage_misc['storage_version']) && isSet(storage_misc['lastversion'])) {
+        storage_misc['storage_version'] = 0;
     }
 
     let is_migrated = false;
     for (const migrate of migrates) {
         const index = migrates.indexOf(migrate);
-        if (parseInt(local['storage_version']) < index + 1) {
-            await migrate(local);
+        if (parseInt(storage_misc['storage_version']) < index + 1) {
+            await migrate(storage_plugins_flat, storage_plugins_user, storage_misc);
             is_migrated = true;
         }
     }
 
-    local['storage_version'] = migrates.length;
-    await storage.set(local);
+    storage_misc['storage_version'] = migrates.length;
+    await storage.set({ ...storage_plugins_flat, ...storage_plugins_user, ...storage_misc });
     return is_migrated;
 }
 
-async function migration_0001(local) {
-    for (let channel of Object.keys(local)) {
-        if (!isSet(local[channel])) continue;
+async function migration_0001(storage_plugins_flat) {
+    for (let channel of Object.keys(storage_plugins_flat)) {
+        if (!isSet(storage_plugins_flat[channel])) continue;
 
-        for (let plugin of Object.keys(local[channel])) {
-            const plugin_obj = local[channel][plugin];
+        for (let plugin of Object.keys(storage_plugins_flat[channel])) {
+            const plugin_obj = storage_plugins_flat[channel][plugin];
 
             if (plugin_obj['user'] === true && plugin_obj['category'] === undefined) {
                 plugin_obj['category'] = 'Misc';
@@ -43,12 +57,28 @@ async function migration_0001(local) {
     }
 }
 
-async function migration_0002(local) {
-    if (['test', 'local'].includes(local.channel)) {
-        local.channel = 'release';
-        local.network_host.custom = local.network_host.local;
+async function migration_0002() {}
+
+async function migration_0003(storage_plugins_flat, storage_plugins_user, storage_misc) {
+    if (['test', 'local'].includes(storage_misc.channel)) {
+        storage_misc.channel = 'release';
+        storage_misc.network_host.custom = storage_misc.network_host.local;
     }
-    if (!['release', 'beta', 'custom'].includes(local.channel)) {
-        local.channel = 'release';
+    if (!['release', 'beta', 'custom'].includes(storage_misc.channel)) {
+        storage_misc.channel = 'release';
+    }
+    for (let channel of Object.keys(storage_plugins_user)) {
+        if (!isSet(storage_plugins_user[channel])) continue;
+
+        for (let plugin of Object.keys(storage_plugins_user[channel])) {
+            const plugin_obj = storage_plugins_user[channel][plugin];
+
+            if (plugin_obj['uid'] === undefined) {
+                plugin_obj['uid'] = getUID(plugin_obj);
+            }
+            if (plugin_obj['status'] === undefined) {
+                plugin_obj['status'] = 'off';
+            }
+        }
     }
 }
--- test/migrations.spec.js
+++ test/migrations.spec.js
@@ -1,37 +1,53 @@
 // @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3
 
-import { describe, it } from 'mocha';
+import { before, describe, it } from 'mocha';
 import { migrate, number_of_migrations } from '../src/migrations.js';
 import storage from '../test/storage.js';
 import { expect } from 'chai';
 
 describe('test migrations', function () {
-    storage.set({
-        release_plugins_flat: {
-            'Bookmarks for maps and portals+https://github.com/IITC-CE/ingress-intel-total-conversion': {
-                id: 'bookmarks',
-                namespace: 'https://github.com/IITC-CE/ingress-intel-total-conversion',
-                name: 'Bookmarks for maps and portals',
-                category: 'Controls',
+    before(async function () {
+        await storage.set({
+            release_plugins_flat: {
+                'Bookmarks for maps and portals+https://github.com/IITC-CE/ingress-intel-total-conversion': {
+                    id: 'bookmarks',
+                    namespace: 'https://github.com/IITC-CE/ingress-intel-total-conversion',
+                    name: 'Bookmarks for maps and portals',
+                    category: 'Controls',
+                },
+                'External+https://github.com/IITC-CE/ingress-intel-total-conversion': {
+                    id: 'ext',
+                    namespace: 'https://github.com/IITC-CE/ingress-intel-total-conversion',
+                    name: 'External plugin with bug https://github.com/IITC-CE/IITC-Button/issues/68',
+                    user: true,
+                    category: undefined,
+                },
             },
-            'External+https://github.com/IITC-CE/ingress-intel-total-conversion': {
-                id: 'ext',
-                namespace: 'https://github.com/IITC-CE/ingress-intel-total-conversion',
-                name: 'External plugin with bug https://github.com/IITC-CE/IITC-Button/issues/68',
-                user: true,
-                category: undefined,
+            release_plugins_user: {
+                'ext+https://github.com/IITC-CE/ingress-intel-total-conversion': {
+                    id: 'bookmarks',
+                    namespace: 'https://github.com/IITC-CE/ingress-intel-total-conversion',
+                    name: 'ext',
+                    category: 'Controls',
+                },
             },
-        },
-        lastversion: '1.7.0',
+            lastversion: '1.7.0',
+            storage_version: 0,
+        });
+        await migrate(storage);
     });
 
-    describe('migrate from v1.7.0', async function () {
-        await migrate(storage);
-
+    describe('check migrations', function () {
         it('Should replace undefined with "Misc" (fix https://github.com/IITC-CE/IITC-Button/issues/68)', async function () {
             const db_data = await storage.get(['release_plugins_flat']);
             const ext_plugin = db_data['release_plugins_flat']['External+https://github.com/IITC-CE/ingress-intel-total-conversion'];
             expect(ext_plugin['category']).to.equal('Misc');
+        });
+        it('Custom plugins should have status and uid', async function () {
+            const db_data = await storage.get(['release_plugins_user']);
+            const ext_plugin = db_data['release_plugins_user']['ext+https://github.com/IITC-CE/ingress-intel-total-conversion'];
+            expect(ext_plugin['uid']).to.equal('ext+https://github.com/IITC-CE/ingress-intel-total-conversion');
+            expect(ext_plugin['status']).to.equal('off');
         });
         it('The value of storage_version field has changed', async function () {
             const db_data = await storage.get(['storage_version']);
